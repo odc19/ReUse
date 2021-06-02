4e0b69468b6f741d7405bfcb5c2bb24ad9b3cf6b
Kt <catalina200029@yahoo.com>
2021-06-02 17:20:53 +0100

    Added button that redirects to another page and passes inputed parameters

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("layout.html")
+    return render_template("index.html")
+
+
+@app.route("/blue/<some_text>")
+def index_blue(some_text):
+    form_text = request.args.get('given_text')
+    return render_template("index_blue.html", given_text=some_text + " " + form_text)
 
 
 if __name__ == "__main__":

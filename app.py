079f6fb7a5b30329037d16d6d599951349d5a6ea
stefania <dat119@ic.ac.uk>
2021-06-02 23:34:30 +0100

    Modified app.py to fix mistakes

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
-from database import cur, conn
 #from flask_sqlalchemy import SQLAlchemy
 
-
+import psycopg2
 #from database import database, Postt
+
+DB_HOST = "ec2-99-80-200-225.eu-west-1.compute.amazonaws.com"
+DB_NAME = "d8t87nco360qgb"
+DB_USER = "tkkjfwcewyiyyy"
+DB_PASS = "45ffc56f105bf668e1ecb8e089261e5d827cd1a43b00f069c75cbf2d2101ca99"
+
 
 
 app = Flask(__name__)
@@ -20,6 +25,8 @@ def index():
 
 @app.route("/blue/<some_text>")
 def index_blue(some_text):
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor()
     from_text = request.args.get('given_text')
     cur.execute("INSERT INTO dummy (text) VALUES(%s)", (from_text,))
     conn.commit()
--- database/__init__.py
+++ database/__init__.py
@@ -4,14 +4,7 @@ import psycopg2
 #import libpq-dev
 #database = SQLAlchemy()
 
-DB_HOST = "ec2-99-80-200-225.eu-west-1.compute.amazonaws.com"
-DB_NAME = "d8t87nco360qgb"
-DB_USER = "tkkjfwcewyiyyy"
-DB_PASS = "45ffc56f105bf668e1ecb8e089261e5d827cd1a43b00f069c75cbf2d2101ca99"
 
-
-conn =psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
-cur = conn.cursor()
 
 #cur.execute("CREATE TABLE example1 (id SERIAL PRIMARY KEY, name VARCHAR)")
 #cur.execute("INSERT INTO mere (name) VALUES(%s)", ("Hello",))

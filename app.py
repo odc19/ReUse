7b7f2cae0f507c09366a3875679d9a2e95b783cd
Kt <catalina200029@yahoo.com>
2021-06-03 20:23:59 +0100

    Created feed with search functionality, added view post button

--- app.py
+++ app.py
@@ -16,28 +16,78 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///EXAMPLE_database.db'
 req_table = "requests"
 don_table = "donations"
 
+
 def init_table_posts():
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
     cur = conn.cursor()
-    #cur.execute(
-    #    "CREATE TABLE donations (id SERIAL PRIMARY KEY, person_id INT, title VARCHAR, category VARCHAR, description VARCHAR, location VARCHAR, reserved VARCHAR);")
-    #cur.execute("CREATE TABLE requests (id SERIAL PRIMARY KEY, person_id INT, title VARCHAR, description VARCHAR, reserved VARCHAR);")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def query_word(word, table, col):
+    # if table == req_table:
+    #     return "SELECT (id, person_id, title, category, description, location, ) FROM " + table + " WHERE " + col + " LIKE '%" + word + "%' ;"
+    # else:
+        return "SELECT * FROM " + table + " WHERE " + col + " LIKE '%" + word + "%' ;"
+
+
+@app.route("/")
+def index():
+    key_word = request.args.get("search_sentence")
+    if not key_word:
+        key_word = ""
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor()
+
+    posts_with_types = []
+
+    cur.execute(query_word(key_word, req_table, "description"))
+    posts = cur.fetchall()
+    for post in posts:
+        posts_with_types.append({"post": post, "type": "request"})
+
+    cur.execute(query_word(key_word, don_table, "description"))
+    posts = cur.fetchall()
+    for post in posts:
+        posts_with_types.append({"post": post, "type": "donation"})
+
+    for post_with_type in posts_with_types:
+        print(post_with_type)
+        print("\n")
 
     conn.commit()
     cur.close()
     conn.close()
 
-def query_word(word, table, col):
-    return "SELECT * FROM " + table + " WHERE " + col + " LIKE '%" + word + "%' ;"
+    return render_template("index.html", posts_with_types=posts_with_types)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/")
+@app.route("/abc")
 def new_post():
     return render_template("new_post.html", given_text="YAAAY!!! FINALLY")
+
+
+@app.route("/post_id=<post_id>/post_type=<post_type>")
+def view_post(post_id, post_type):
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor()
+
+    if post_type == "donation":
+        table = don_table
+        print("DONATION\n")
+    else:
+        table = req_table
+        print("REQUEST\n")
+    cur.execute("SELECT * FROM " + table + " WHERE id = " + post_id + ";")
+    post = cur.fetchone()
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return render_template("view_post.html", post=post, post_type=post_type)
+
 
 def get_table_rows(table):
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
@@ -51,44 +101,6 @@ def get_table_rows(table):
     cur.close()
     conn.close()
     return rows_nr
-
-@app.route("/blue/<some_text>")
-def index_blue(some_text):
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
-    cur = conn.cursor()
-    from_text = request.args.get('given_text')
-    # cur.execute("INSERT INTO dummy (text) VALUES(%s)", (from_text,))
-    res = ""
-    cur.execute(query_word(from_text, don_table, "description"))
-    rows = get_table_rows(don_table)
-    # Pana aici e bine
-    if rows > 1:
-        row = cur.fetchone()
-        res1 = row
-        row = cur.fetchone()
-        res2 = row
-
-    #cur.execute(query_word(from_text, req_table, "description"))
-
-    """
-    res1 = cur.fetchall()
-    res = ""
-    for r in res1:
-        res += r[2] + " " + r[3]
-    cur.execute(query_word(from_text, req_table, "description"))
-    res2 = cur.fetchall()
-
-    res += "\n"
-
-    for r in res2:
-        res += r[2] + " " + r[3]
-   # res = res1 + res2
-   """
-    conn.commit()
-    cur.close()
-    conn.close()
-    # return render_template("index_blue.html", given_text=some_text + " " + from_text)
-    return render_template("index_blue.html", given_text=res)
 
 
 if __name__ == "__main__":

2406724f5cbb91ba409ad65cb96ce2fc70cbdb1b
Cuziac, Radu <radu.cuziac19@imperial.ac.uk>
2021-06-03 23:42:57 +0000

    Delete tests_db_query.py

--- tests/tests_db_query.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-from ..app import DB_HOST, DB_NAME, DB_PASS, DB_USER, req_table, don_table
-
-def adds_elem_in_db():
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
-    cur = conn.cursor()
-    conn.commit()
-    cur.close()
-    conn.close()

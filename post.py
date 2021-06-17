a5871d7b73116e765ca258fdc986838e6c98a7d8
rac119 <rac119@ic.ac.uk>
2021-06-17 19:14:20 +0300

    decreased time for searching the filters

--- post.py
+++ post.py
@@ -1,5 +1,5 @@
 class post:
-    def __init__(self, id, person_id, title, category, description, location, reserved, date, is_charity):
+    def __init__(self, id, person_id, title, category, description, location, reserved, date, is_charity, lat, lng):
         self.id = id
         self.person_id = person_id
         self.title = title
@@ -9,16 +9,18 @@ class post:
         self.reserved = reserved
         self.date = date
         self.is_charity = is_charity
+        self.lat = lat
+        self.lng = lng
 
 
 class post_request(post):
-    def __init__(self, id, person_id, title, category, description, location, reserved, date, is_charity):
-        post.__init__(self, id, person_id, title, category, description, location, reserved, date, is_charity)
+    def __init__(self, id, person_id, title, category, description, location, reserved, date, is_charity, lat, lng):
+        post.__init__(self, id, person_id, title, category, description, location, reserved, date, is_charity, lat, lng)
 
 
 class post_donation(post):
     def __init__(self, id, person_id, title, category, description, location, reserved, date, is_charity,
-                 condition, condition_description):
-        post.__init__(self, id, person_id, title, category, description, location, reserved, date, is_charity)
+                 condition, condition_description, lat, lng):
+        post.__init__(self, id, person_id, title, category, description, location, reserved, date, is_charity, lat, lng)
         self.condition = condition
         self.condition_description = condition_description
